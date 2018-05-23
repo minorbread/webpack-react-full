de8923c54b4bcc6ce035cf2b8aeb99883acec528
minorbread <hmcdl@foxmail.com>
2018-05-23 11:57:24 +0800

    dev server SSr

--- server/server.js
+++ server/server.js
@@ -1,21 +1,33 @@
 const express = require('express')
 const ReactSSR = require('react-dom/server')
-const serverEntry = require('../dist/server-entry').default
 const fs = require('fs')
 const path = require('path')
 
+// 判断生产或者开发模式
+const isDev = process.env.NODE_ENV === 'development'
 
-const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8');
 
 // console.log(serverEntry); 读取服务器端需要渲染的代码
 const app = express()
 
-app.use('/public', express.static(path.join(__dirname, '../dist')))
 
-app.get('*', function (req, res) {
-  const appString = ReactSSR.renderToString(serverEntry)
-  res.send(template.replace('<!-- app -->', appString))
-})
+
+if (!isDev) {
+  // 不是开发环境下
+  const serverEntry = require('../dist/server-entry').default
+  const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8');
+  
+  app.use('/public', express.static(path.join(__dirname, '../dist')))
+
+  app.get('*', function (req, res) {
+    const appString = ReactSSR.renderToString(serverEntry)
+    res.send(template.replace('<!-- app -->', appString))
+  })
+} else {
+  // 开发环境下
+  const devStatic = require('./util/dev-static')
+  devStatic(app)
+}
 
 app.listen(3333, function () {
   console.log('server is listening on 3333');
---
+++ server/util/dev-static.js
@@ -0,0 +1,76 @@
+// 开发环境下的服务端渲染
+const axios = require('axios')
+const webpack = require('webpack')
+const path = require('path')
+// 在内存里读写文件
+const MemoryFs = require('memory-fs')
+
+// 代理
+const proxy = require('http-proxy-middleware')
+
+const ReactDomServer = require('react-dom/server')
+
+const serverConfig = require('../../build/webpack.config.server')
+
+let serverBundle
+
+// 读取模板 
+const getTemplate = () => {
+  return new Promise((resolve, reject) => {
+    axios.get('http://localhost:8888/public/index.html')
+      .then(res => {
+        resolve(res.data)
+      })
+      .catch(reject)
+  })
+}
+
+// hack? module.export
+const Module = module.constructor
+
+// 服务器端编译
+const mfs = new MemoryFs()
+// 需要使用到的内容
+const serverCompiler = webpack(serverConfig)
+// 加快打包速度
+serverCompiler.outputFileSystem = mfs
+serverCompiler.watch({}, (err, stats) => {
+  if (err) {
+    throw err
+  }
+  // webpack打包 转化后显示信息
+  stats = stats.toJson()
+  stats.errors.forEach(err => console.error(err))
+  stats.warnings.forEach(warn => console.warn(warn))
+
+  const bundlePath = path.join(
+    serverConfig.output.path,
+    serverConfig.output.filename 
+  )
+
+  // 内存???
+  const bundle = mfs.readFileSync(bundlePath, 'utf-8')
+  const m = new Module()
+  // 蜜汁bug            指定文件名
+  m._compile(bundle, 'server-entry.js')
+  // commonjs ?
+  serverBundle = m.exports.default
+
+
+})
+
+module.exports = function (app) {
+
+  app.use('/public', proxy({
+    target: 'http://localhost:8888/'
+  }))
+
+
+  app.get('*', function (req, res) {
+    getTemplate().then(templace => {
+      const content = ReactDomServer.renderToString(serverBundle)
+      res.send(templace.replace('<!-- app -->', content))
+    })
+  })
+
+}
